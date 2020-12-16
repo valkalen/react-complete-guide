2cd1c26b68f3620cd7577d83877ecf4b20c72edc
Val <kalenikvalentin@yahoo.com>
2020-12-15 18:59:30 -0600

    Commit Test

--- src/App.js
+++ src/App.js
@@ -1,69 +1,7 @@
-
-//Hooks way
-// import React, { useState } from 'react';
-// import './App.css';
-// import Person from './Person/Person';
-
-// const app = () =>{ 
-//   const [personsState, setPersonsState] = useState({
-//     persons: [
-//       {name: "Val", age: 28},
-//       {name: "Max", age: 29},
-//       {name: "Karen", age: 30}
-//     ],
-//     otherState:  'hey'
-//   });
-
-//You can have as many useState cases as you want
-// Hooks completly replace NOT merge state. Make sure you don't loose your other state. Work around is to create multiple useStates
-//   const switchNameHandler = () => {
-//   setPersonsState({
-//     persons: [
-//       {name: "Valentino", age: 28},
-//       {name: "Max", age: 29},
-//       {name: "Karen", age: 31}
-//     ]
-//   })}
-
-//   return (
-//     <div className="App">
-//       <h1>Hi, I'm a React App</h1>
-//       <button onClick={switchNameHandler}>Switch Name</button>
-//       <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>My Hobbies: none</Person>
-//       <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
-//       <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>My Hobbies: Snowboarding</Person>
-//     </div>
-//   );
-//   // return React.createElement('div',{className: 'App'}, React.createElement('h1', null, 'Hello Val!'));
-
-// }
-
-// export default app;
-
-
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 import classes from './App.module.css'
-//import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
-//import styled from 'styled-components';
-//import Radium, {StyleRoot} from 'radium';
-
-
-
-// const StyledButton = styled.button`
-//     background-color: ${props => props.alt? 'red': 'green'};
-//     color: white;
-//     font: inherit;npm run 
-//     border: 1px solid blue;
-//     padding: 8px;
-//     cursor: pointer;
-
-//     &:hover {
-//       background-color: ${props => props.alt? 'salmon': 'lightgreen'};
-//       color: black;
-//     }
-//   `;
 
 
 class App extends Component {
@@ -78,7 +16,6 @@ class App extends Component {
   }
 
   switchNameHandler = (newName) => {
-    //console.log('Was clicked');
     this.setState({
       persons: [
         { name: newName, age: 28 },
@@ -88,7 +25,6 @@ class App extends Component {
     })
   }
   deletePersonHandler = (index) => {
-    //const persons = this.state.persons.slice();
     const persons = [...this.state.persons]; //copy the array first, slice() without args does the same thing
     persons.splice(index, 1);
     this.setState({ persons: persons });
@@ -97,7 +33,6 @@ class App extends Component {
   nameChangeHandler = (event, id) => {
     const personIndex = this.state.persons.findIndex(p => p.id === id);
     const person = { ...this.state.persons[personIndex] }; //person is an object
-    //const person = Object.assign({}, this.state.persons[personIndex]); alternative to the methond above
     person.name = event.target.value;
     const persons = [...this.state.persons];
     persons[personIndex] = person;
@@ -113,18 +48,7 @@ class App extends Component {
 
 
   render() {
-    // const style = {
-    //   backgroundColor: 'green',
-    //   color: 'white',
-    //   font: 'inherit',
-    //   border: '1px solid blue',
-    //   padding: '8px',
-    //   cursor: 'pointer',
-    //   ':hover': {
-    //     backgroundColor: 'lightgreen',
-    //     color: 'black'
-    //   }
-    // };
+
     let btnClass = '';
 
     let persons = null;
@@ -132,7 +56,6 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            //return <ErrorBoundary key={person.id}>
             return <Person
               key={person.id}
               name={person.name}
@@ -140,20 +63,13 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               changed={(event) => this.nameChangeHandler(event, person.id)}
             />
-            //</ErrorBoundary>
           })}
         </div>
       );
-      // style.backgroundColor = 'red';
-      // style[':hover'] = {
-      //   backgroundColor: 'salmon',
-      //   color: 'black'
-      // }
+
 
       btnClass = classes.Red;
     }
-
-    //let classes = ['red', 'bold'].join(' ');
 
     const assignedClasses = [];
     if (this.state.persons.length <= 2) {
@@ -164,47 +80,24 @@ class App extends Component {
     }
 
     return (
-      //<StyleRoot>
       <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
         <p className={assignedClasses.join(' ')}>This is really working!!!</p>
         <button
-          //style={style}
           className={btnClass}
-          //key='key1'
-          //alt = {this.state.showPersons}
           onClick={this.switchNameHandler.bind(this, "Valik")}>Switch Name
           </button>
         <button
           className={btnClass}
-          //key='key2'
-          // alt = {this.state.showPersons}
           onClick={this.togglePersonsHandler}>Toggle Person
-            {/* or you can do this onClick={()=>this.togglePersonsHandler()}>Toggle Person*/}
         </button>
         {persons}
-        {/* alternative syntax
-          this.state.showPersons === true?
-          <div>
-          <Person 
-              name={this.state.persons[0].name} 
-              age={this.state.persons[0].age}>My Hobbies: none</Person>
-          <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age}
-              click={this.switchNameHandler.bind(this, "Valentin")}
-              changed={this.nameChangeHandler}/>
-          <Person 
-              name={this.state.persons[2].name} 
-              age={this.state.persons[2].age}>My Hobbies: Snowboarding</Person>
-        </div> : null*/
-        }
+       
       </div>
-      //  </StyleRoot>
+    
     );
-    // return React.createElement('div',{className: 'App'}, React.createElement('h1', null, 'Hello Val!'));
+  
   }
 }
 
-//export default Radium(App);
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,44 +2,16 @@ import React from 'react';
 import './Person.css';
 import classes from './Person.module.css'
 
-//import styled from 'styled-components';
-//import Radium from 'radium'
 
-// const StyleDiv = styled.div`
-//     width: 60%;
-//     margin: 16px auto;
-//     border: 1px solid #eee;
-//     box-shadow: 0 2px 3px #ccc;
-//     padding: 16px;
-//     text-align: center;
-
-//     @media (min-width: 500px) {
-//         width: 450px;   
-//     }
-// `;
 
 const person = (props) => {
-    // const style = {
-    //     '@media (min-width: 500px)' : {
-    //         width: '450px'
-    //     }
-    // }
-
-    // const rnd = Math.random();
-    // if(rnd > 0.7){
-    //     throw new Error('Something went wrong.');
-    // }
     return (
-        //<div className="Person" style={style}>
-        // <StyleDiv>
         <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}></input>
         </div>
-        // </StyleDiv>
+
     )
 }
-
-//export default Radium(person);
 export default person;
