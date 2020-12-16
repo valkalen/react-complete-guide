7b825a6f9c00716e567a0589c64938c9a7a2b139
Val <kalenikvalentin@yahoo.com>
2020-12-16 12:39:37 -0600

    Lifecycle hooks Update

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,28 +1,38 @@
 import React from 'react';
 import Person from './Person/Person';
 
-// const persons = (props) => props.persons.map((person, index) => {
-//     return <Person
-//       key={person.id}
-//       name={person.name}
-//       age={person.age}
-//       click={() => props.clicked(index)}
-//       changed={(event) => props.changed(event, person.id)}
-//     />
-//   });
+class Persons extends React.Component {
+  // static getDerivedStateFromProps(props, state){
+  //   console.log('[Persons.js] getDerivedStateFromProps', props);
+  //   return state;
+  // }
 
-const persons = (props) => {
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('[Persons.js shouldComponentUpdate]',nextProps, nextState);
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState){
+    console.log('[Persons.js] getShapShotBeforeUpdate');
+    return {mgs: 'Sanpshot!'};
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot){
+    console.log('[Persons.js] componentDidUpdate', prevProps, prevState, snapshot);
+  }
+  render() {
     console.log('[Persons.js rendering...]');
-    return props.persons.map((person, index) => {
-    return (<Person
-      key={person.id}
-      name={person.name}
-      age={person.age}
-      click={() => props.clicked(index)}
-      changed={(event) => props.changed(event, person.id)}
-    />
-  )
-})
+    return this.props.persons.map((person, index) => {
+      return (<Person
+        key={person.id}
+        name={person.name}
+        age={person.age}
+        click={() => this.props.clicked(index)}
+        changed={(event) => this.props.changed(event, person.id)}
+      />
+      )
+    })
+  }
 };
 
-export default persons;
+export default Persons;
--- src/containers/App.js
+++ src/containers/App.js
@@ -35,6 +35,14 @@ class App extends Component {
     console.log('[App.js] componentDidMount');
   }
 
+  shouldComponentUpdate(nextProps, nextState){
+    console.log('[App.js] shouldComponentUpdate');
+    return true;
+  }
+
+  componentDidUpdate(){
+    console.log('[App.js] componentDidUpdate');
+  }
   deletePersonHandler = (index) => {
     const persons = [...this.state.persons]; //copy the array first, slice() without args does the same thing
     persons.splice(index, 1);
